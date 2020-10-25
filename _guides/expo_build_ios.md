d0b27555f214489322ca8b378c59da8359afc788
Roman Vavilov <rvavilov@clarus.lan>
2020-10-25 21:22:06 +0300

    nav

--- _guides/expo_build_ios.md
+++ _guides/expo_build_ios.md
@@ -2,10 +2,17 @@
 layout: default
 title: IOS Expo
 ---
-Для сборки приложения для Xcode simulator:<br>
-```bash turtle build:ios --type simulator --config app.json --output artifact/application_name.tar.gz -u expo_login -p expo_password```
+### Cборка приложения для **Xcode simulator**:<br>
+<br>
+
+```bash
+turtle build:ios --type simulator --config app.json --output artifact/application_name.tar.gz -u expo_login -p expo_password
+```
+
+```bash
 DEVELOPER_DIR=/Applications/Xcode11.3.1.app/Contents/Developer/ \
 EXPO_USERNAME=expo_username \
 EXPO_PASSWORD=expo_password \
 EXPO_IOS_DIST_P12_PASSWORD=cert_password \
 npm run build-ios-archive
+```
--- assets/js/main.js
+++ assets/js/main.js
@@ -12,31 +12,36 @@ var sectionHeight = function() {
 
 $(window).resize(sectionHeight);
 
-var selectedSection = window.localstorage !== undefined && window.localstorage.getItem('selectedGuide');
-selectedSection = !selectedSection ? 0 : parseInt(selectedSection);
+var selectedSection = 0;
+
+$(window).ready(function() {
+  selectedSection =
+    window.localStorage !== undefined
+    ? window.localStorage.getItem('selectedGuide')
+    : 0;
+});
+
+selectedSection = parseInt(selectedSection);
 
 $(function() {
   $("nav li").each(function( index ){
     $(this).addClass('tag-' + this.nodeName.toLowerCase());
     // $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    console.log($(this));
-    console.log(index);
-    console.log(selectedSection);
-    console.log('index === selectedSection', index === parseInt(selectedSection));
-    if (index === selectedSection) {
-      console.log('@@@@@')
+    $(this).attr("id", index);
+    if (index === parseInt(selectedSection)) {
       $(this).addClass("active");
+      const offset = $(this).children("a").offset();
+      var position = offset.top - 190;
+      $("html, body").animate({scrollTo: position}, 400);
     }
+
+    $("nav ul li").on("click", "a", function(event) {
+      window.localStorage.setItem('selectedGuide', $(this).parent().attr('id'));
+      localstorage.setItem('selectedGuide', index.toString());
+      event.preventDefault();
+    });
   });
 
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTo: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();
-  });
 
   $('img').on('load', sectionHeight);
 });
